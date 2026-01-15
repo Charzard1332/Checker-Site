efbac91bb624f2c7b9798c2d4998d1aed0854dbe
Charzard1332 <charchar7256@icloud.com>
2026-01-14 22:12:38 -0600

    Initial commit

---
+++ Checker-Site/Middleware/SecurityMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http; // including http services lib
+
+namespace Checker_Site.Middleware;
+
+// middleware to add security headers and enforce security policies
+public class SecurityHeadersMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<SecurityHeadersMiddleware> _loggerFactory;
+
+    public SecurityHeadersMiddlware(RequestDelegate next, ILogger<SecurityHeadersMiddleware> iLogger)
+    {
+        _next = next;
+        _loggerFactory = iLogger;
+    }
+}
