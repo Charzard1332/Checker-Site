f4bdace5e2008f1ed8dd728fb9546fa30c4d7eb1
Charzard1332 <charchar7256@icloud.com>
2026-01-15 10:46:33 -0600

    Models: Finished BatchCheckRequest.cs this is the request model for checking multiple accounts

---
+++ Checker-Site-Shared/Models/BatchCheckRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Checker_Site_Shared.Models;
+
+// request model for checking multiple accounts
+public class BatchCheckRequest
+{
+    // list of accounts to check
+    public List<AccountCheckRequest> Accounts { get; set; } = new();
+    
+    // service types for all accounts
+    public string? ServiceType { get; set; }
+    
+    // max concurrent checks (default = 5)
+    public int? MaxConcurrentChecks { get; set; }
+}
