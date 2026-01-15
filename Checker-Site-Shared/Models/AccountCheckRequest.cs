9194d7db66ed942ab7c5b62aea5a3120c02af6af
Charzard1332 <charchar7256@icloud.com>
2026-01-15 10:40:20 -0600

    Models: Finished AccountCheckRequest.cs this is basic stuff for single account checking not batch checking [email,password,proxy] outputting serviceType (Epic, Steam, Discord etc)

---
+++ Checker-Site-Shared/Models/AccountCheckRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Checker_Site_Shared.Models;
+
+// request model for checking a single account (not needed but its handy)
+public class AccountCheckRequest
+{
+    // username/email to check
+    public string Email { get; set; } = string.Empty;
+    
+    // password for the account
+    public string Password { get; set; } = string.Empty;
+    
+    // service type like (Epic, Steam, Discord, etc..)
+    public string? ServiceType { get; set; }
+    
+    // this is optional and not needed. but here it is anyway
+    // proxy to use for the request
+    public string? Proxy { get; set; }
+}
